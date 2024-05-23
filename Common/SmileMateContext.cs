b72b5def9fa8321b7e93f77a64ce47012db8d93c
MarkusAnders <ser.play02@mail.ru>
2024-05-23 19:22:47 +0300

    Добавьте файлы проекта.

---
+++ Common/SmileMateContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SmileMate.Common
+{
+    public class SmileMateContext : DbContext
+    {
+        public SmileMateContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=CoursePlatform;Username=postgres;Password=admin");
+        }
+    }
+}
